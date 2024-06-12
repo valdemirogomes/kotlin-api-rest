b2d2eff7402db18e9f154ba53f32872a847566cf
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2024-06-12 00:53:02 -0300

    API REST in Kotlin

---
+++ src/main/kotlin/com/api/account/ApiAccountApplication.kt
@@ -0,0 +1,11 @@
+package com.api.account
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiAccountApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiAccountApplication>(*args)
+}
---
+++ src/test/kotlin/com/api/account/ApiAccountApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.account
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiAccountApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
