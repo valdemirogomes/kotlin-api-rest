584dbb77af0c8f79071d290477e10345227186b4
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2024-06-13 19:36:36 -0300

    implementing function getAll

--- src/main/kotlin/com/api/account/AccountController.kt
+++ src/main/kotlin/com/api/account/AccountController.kt
@@ -3,7 +3,6 @@ package com.api.account
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,10 +10,14 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/accounts")
-class AccountController(private val repository: AccountInterface) {
+class AccountController(private val repository: AccountInterfaceRepository) {
 
     @PostMapping
     fun create(@RequestBody account: Account): ResponseEntity<Account> =
        ResponseEntity.status(HttpStatus.CREATED).body(repository.save(account));
+
+    @GetMapping
+    fun getAll(): ResponseEntity<List<Account>> =
+        ResponseEntity.ok(repository.findAll())
 
 }
